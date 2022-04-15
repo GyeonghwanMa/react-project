330fba91bc61a1845c427e06a4dbb63434801843
Gyeonghwan <ghma1213@gmail.com>
2022-04-15 17:31:15 +0900

    css 수정

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -3,9 +3,11 @@ import React from "react";
 function UserInfo({_id, email, name}: any) {
     return (
         <div>
-            <p>_id: {_id}</p>
-            <p>email: {email}</p>
-            <p>name: {name}</p>
+            <div style={{border: "1px solid grey", borderRadius: 10, padding: 10, margin: 10}}>
+                <p>_id: {_id}</p>
+                <p>email: {email}</p>
+                <p>name: {name}</p>
+            </div>
         </div>
     )
 }
